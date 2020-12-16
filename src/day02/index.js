331c69e20af13569788200fa380c0983f94e887f
chrispiccaro <chrispiccaro18@gmail.com>
2020-12-16 11:22:24 -0800

    day02 part2

--- src/day02/index.js
+++ src/day02/index.js
@@ -2,19 +2,41 @@ const input = require('./input');
 
 const inputArray = input.split('\n');
 
+// part 1
+// const validPasswords = inputArray.reduce((validCount, input) => {
+//   const [min, rest] = input.split('-');
+//   const [max, givenLetterWithColon, password] = rest.split(' ');
+//   const givenLetter = givenLetterWithColon[0];
+
+//   const letterRegex = new RegExp(givenLetter, 'g');
+//   const countLetterAppearsInPassword = [...password.matchAll(letterRegex)].length;
+
+//   if(countLetterAppearsInPassword >= min && countLetterAppearsInPassword <= max)
+//     validCount++;
+
+//   return validCount;
+// }, 0);
+
+// console.log(validPasswords);
+// 383
+
+// part 2
+// numbers refer to non-zero index
 const validPasswords = inputArray.reduce((validCount, input) => {
-  const [min, rest] = input.split('-');
-  const [max, givenLetterWithColon, password] = rest.split(' ');
+  const [firstIndex, rest] = input.split('-');
+  const [secondIndex, givenLetterWithColon, password] = rest.split(' ');
   const givenLetter = givenLetterWithColon[0];
 
-  const letterRegex = new RegExp(givenLetter, 'g');
-  const countLetterAppearsInPassword = [...password.matchAll(letterRegex)].length;
+  const isFirstIndex = password[firstIndex - 1] === givenLetter;
+  const isSecondIndex = password[secondIndex - 1] === givenLetter;
 
-  if(countLetterAppearsInPassword >= min && countLetterAppearsInPassword <= max)
-    validCount++;
+  // logical XOR
+  if(isFirstIndex || isSecondIndex) {
+    if(isFirstIndex !== isSecondIndex) validCount++;
+  }
 
   return validCount;
 }, 0);
 
 console.log(validPasswords);
-// 383
+// 272
