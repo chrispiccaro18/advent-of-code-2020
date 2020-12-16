d06b52ae3b80fc31cd185455883aa738e2be77fb
chrispiccaro <chrispiccaro18@gmail.com>
2020-12-16 12:30:44 -0800

    day03 part2

--- src/day03/index.js
+++ src/day03/index.js
@@ -5,12 +5,50 @@ const TREE = '#';
 const inputArray = input.split('\n');
 const patchWidth = inputArray[0].length;
 
-let treesHit = 0;
+// let treesHit = 0;
 
-for(let i = 1, j = 3; i < inputArray.length; i++, j = j + 3) {
-  const verticalTreeLine = inputArray[i];
-  if(verticalTreeLine[j % patchWidth] === TREE) treesHit++;
+// part 1
+// for(let i = 1, j = 3; i < inputArray.length; i++, j = j + 3) {
+//   const verticalTreeLine = inputArray[i];
+//   if(verticalTreeLine[j % patchWidth] === TREE) treesHit++;
+// }
+
+// console.log(treesHit);
+// 145
+
+// part 2
+const multiplyArray = arrayOfNumbers => {
+  return arrayOfNumbers.reduce((product, number) => {
+    if(!product) return number;
+    else return product * number;
+  }, null);
+};
+
+const slopes = [
+  // right, down
+  [1, 1],
+  [3, 1],
+  [5, 1],
+  [7, 1],
+  [1, 2],
+];
+
+const totalTreesHit = [];
+
+for(let i = 0; i < slopes.length; i++) {
+  const [rightSlope, downSlope] = slopes[i];
+  let treesHit = 0;
+  
+  for(let j = downSlope, k = rightSlope;
+    j < inputArray.length;
+    j = j + downSlope, k = k + rightSlope)
+  {
+    const verticalTreeLine = inputArray[j];
+    if(verticalTreeLine[k % patchWidth] === TREE) treesHit++;
+  }
+
+  totalTreesHit.push(treesHit);
 }
 
-console.log(treesHit);
-// 145
+console.log(multiplyArray(totalTreesHit));
+// 3424528800
