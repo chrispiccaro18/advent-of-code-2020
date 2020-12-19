b803916f7a24d0f8923eb64716abc91dd8756375
chrispiccaro <chrispiccaro18@gmail.com>
2020-12-19 15:38:13 -0800

    day05 part2

--- src/day05/index.js
+++ src/day05/index.js
@@ -27,11 +27,40 @@ const decodeBoardingPass = boardingPass => {
 
 const calculateSeatId = (row, column) => row * 8 + column;
 
-const highestSeatId = inputArray.reduce((highestId, boardingPass) => {
+// const highestSeatId = inputArray.reduce((highestId, boardingPass) => {
+//   const [row, column] = decodeBoardingPass(boardingPass);
+//   const currentSeatId = calculateSeatId(row, column);
+//   if(currentSeatId > highestId) return currentSeatId;
+//   else return highestId;
+// }, 0);
+
+// console.log(highestSeatId);
+// 806
+
+// part 2
+// find lowest and highest 
+// then find missing in between
+// creat seatId array
+// loop from lowest to highest making sure each id is in the array
+// seatId not in the array must be our seatId
+
+const seatIdInfo = inputArray.reduce(({ lowest, highest, seatIds }, boardingPass) => {
   const [row, column] = decodeBoardingPass(boardingPass);
   const currentSeatId = calculateSeatId(row, column);
-  if(currentSeatId > highestId) return currentSeatId;
-  else return highestId;
-}, 0);
 
-console.log(highestSeatId);
+  seatIds.push(currentSeatId);
+  if(currentSeatId > highest) highest = currentSeatId;
+  if(currentSeatId < lowest) lowest = currentSeatId;
+  
+  return { lowest, highest, seatIds };
+}, {
+  lowest: 1032,
+  highest: 0,
+  seatIds: [],
+});
+
+const { lowest, highest, seatIds } = seatIdInfo;
+
+for(let i = lowest; i < highest + 1; i++) {
+  if(!seatIds.includes(i)) console.log(i);
+}
