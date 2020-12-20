892f780a5e02e7a168e1049505f01488655e139b
chrispiccaro <chrispiccaro18@gmail.com>
2020-12-20 12:47:22 -0800

    day06 part2

--- src/day06/index.js
+++ src/day06/index.js
@@ -1,21 +1,64 @@
 const input = require('./input');
 
+// const input = `abc
+
+// a
+// b
+// c
+
+// ab
+// ac
+
+// a
+// a
+// a
+// a
+
+// b`;
+
 const inputArray = input.split(/\n\s*\n/);
 
-const totalYesCount = inputArray.reduce((yesCount, group) => {
-  const groupsAnswers = [];
-  const groupAnswerArray = group.split('\n');
+// part 1
+// const totalYesCount = inputArray.reduce((yesCount, group) => {
+//   const groupsAnswers = [];
+//   const groupAnswerArray = group.split('\n');
 
   
-  for(let i = 0; i < groupAnswerArray.length; i++) {
+//   for(let i = 0; i < groupAnswerArray.length; i++) {
+//     const individualsAnswers = groupAnswerArray[i].split('');
+    
+//     for(let j = 0; j < individualsAnswers.length; j++) {
+//       const answer = individualsAnswers[j];
+//       if(!groupsAnswers.includes(answer)) groupsAnswers.push(answer);
+//     }
+//   }
+//   return yesCount + groupsAnswers.length;
+// }, 0);
+
+// console.log(totalYesCount);
+
+// part 2
+const totalYesCount = inputArray.reduce((yesCount, group) => {
+  const groupsAnswers = new Map;
+  const groupAnswerArray = group.split('\n');
+  const lengthOfGroup = groupAnswerArray.length;
+  
+  for(let i = 0; i < lengthOfGroup; i++) {
     const individualsAnswers = groupAnswerArray[i].split('');
     
     for(let j = 0; j < individualsAnswers.length; j++) {
       const answer = individualsAnswers[j];
-      if(!groupsAnswers.includes(answer)) groupsAnswers.push(answer);
+      if(!groupsAnswers.has(answer)) groupsAnswers.set(answer, 1);
+      else groupsAnswers.set(answer, groupsAnswers.get(answer) + 1);
     }
   }
-  return yesCount + groupsAnswers.length;
+
+  let groupYesCount = 0;
+  groupsAnswers.forEach(numberOfYess => {
+    if(numberOfYess === lengthOfGroup) groupYesCount++;
+  });
+
+  return yesCount + groupYesCount;
 }, 0);
 
 console.log(totalYesCount);
